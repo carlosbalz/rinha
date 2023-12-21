bb5824855ca4555eb60cd014a0a9f5136cb72605
carlosbalz <carlosmoraes.tm@gmail.com>
2023-12-21 00:16:35 -0300

    script para criar a tabela de pessoa está rodando

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS pessoa (
+    id          SERIAL PRIMARY KEY,
+    apelido     VARCHAR(32) NOT NULL,
+    nome        VARCHAR(100) NOT NULL,
+    nascimento  VARCHAR(10) NOT NULL,
+    stack       VARCHAR(32)
+);
