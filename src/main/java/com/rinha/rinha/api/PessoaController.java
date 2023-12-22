00198aea13af4a295f7897ff1ab02a8998c83166
carlosbalz <carlosmoraes.tm@gmail.com>
2023-12-22 02:07:50 -0300

    tá dando ruim pra executar a query

--- src/main/java/com/rinha/rinha/api/PessoaController.java
+++ src/main/java/com/rinha/rinha/api/PessoaController.java
@@ -3,6 +3,7 @@ package com.rinha.rinha.api;
 import com.rinha.rinha.model.Pessoa;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +32,13 @@ public class PessoaController {
 
     @GetMapping("/contagem-pessoas")
     public int countPessoas() {
+        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/rinha","cbalz","123")) {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT COUNT(*) AS TOTAL FROM PESSOA");
+            return result.getInt("total");
+        } catch (SQLException e) {
+
+        }
         return 0;
     }
 }
